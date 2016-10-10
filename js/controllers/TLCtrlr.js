7d3091fa26147ab1bc314eb97f2c519225be72a6
Joey <joeychung960@gmail.com>
2016-10-10 09:14:10 -0700

    Added in non-persistent add and remove logic. I'll need to store locally later to allow data to move between refreshes and site navigation.

--- js/controllers/TLCtrlr.js
+++ js/controllers/TLCtrlr.js
@@ -1,10 +1,32 @@
-angular.module('myTaskList')
-    .controller('tlCtrl', function($scope)) {
-        var tlist = $scope.tlist;
+angular.module('myTasks')
+    .controller('tlCtrl', function($scope) {
+        $scope.tasks = [{currentTask:'Complete me!', complete:false}];
+    
+        var tasks = $scope.tasks;
         
-        var newTask = '';
-        
-        $scope.addTask = function () {
+        $scope.newTask = "I'm a task!";
 
+        $scope.addTask = function() {
+            var task = {
+                currentTask: $scope.newTask.trim(),
+                complete: false
+            }
+            if (!$scope.newTask) {
+                return ;
+            }
+            $scope.tasks.push(task);
+            $scope.newTask = "";
         };
-    }
+        
+        $scope.editTask = function() {
+        
+        };
+
+        $scope.completeTask = function() {
+            angular.forEach($scope.todoList, function(task) {
+            if (task.done) {
+                $scope.todoList.splice($scope.todoList.indexOf(task),1)
+                }
+            });
+        };
+    });
